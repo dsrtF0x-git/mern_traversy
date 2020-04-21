7e2970b1b2512a9e06178854fee03a91fadb1ed9
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-04-21 12:25:16 +0300

    basic start

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.get("/", (req, res) => {
+  res.send("Its running")
+})
+
+app.listen(PORT, () => console.log("Server is running...."))
