2d973535eb2ca79ab8d5d65af4d44a62a809dbbf
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-04-28 13:59:46 +0300

    added middleware for authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../../models/User.js");
+const auth = require("../../middleware/auth.js");
 // @route api/auth
-router.get("/", (req, res) => {
-  res.send("Auth route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.status(500).send("Server error.")
+  }
 });
 
 module.exports = router;
