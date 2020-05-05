496c54b61ae17882302bf10065b3bf5ec470290a
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-05-05 15:19:52 +0300

    added experience delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -174,4 +174,22 @@ router.put("/experience", [ auth, [
   }
 });
 
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
+
 module.exports = router;
