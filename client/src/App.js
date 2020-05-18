c2e30518fa52c916112a8cdda7722c02718571ce
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-05-18 19:22:26 +0300

    created store

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,25 @@ import Navbar from './components/layout/Navbar';
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from './components/layout/Landing';
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-    <>
-      <Navbar />
-      <Route  exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
   );
 }
 
