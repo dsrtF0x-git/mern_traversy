9bf0d6afe15927890964a15aa4835d58e1cbdbf9
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-05-26 09:32:23 +0300

    first line
    second line
    
    third line

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
 
 
-const Dashboard = () => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return (
     <div>
       Dashboard
@@ -12,8 +19,14 @@ const Dashboard = () => {
 
 
 Dashboard.propTypes = {
-
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
 
-export default Dashboard;
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
