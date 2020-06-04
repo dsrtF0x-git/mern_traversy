d93ae0fc271c60f1dcacefa1db6102f4e180288f
dsrtF0x-git <gespenst.serge@yandex.ru>
2020-06-04 09:10:58 +0300

    adfsdf

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
+import { getProfiles } from '../../actions/profile';
+import ProfileItem from './ProfileItem';
+import { connect } from 'react-redux';
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className='large text-primary'>Developers</h1>
+          <p>Browse and connect with developers</p>
+          <div>
+            {profiles.length ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found</h4>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func,
+  profile: PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
